7e657560212d5f2bde2c45566bf669840e9c9828
uzair-muaz <uzair.muaz3k@gmail.com>
2023-10-14 19:55:31 +0500

    Bottombar Completed

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
 function Bottombar() {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
-    <div>Bottombar</div>
-  )
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                width={24}
+                height={24}
+                src={link.imgURL}
+                alt={link.label}
+              />
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                {link.label.split(' ')[0]}
+                </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
-export default Bottombar
+export default Bottombar;
